68f235e120f0f2d3c4449d1112dccf8c1f863df9
AWAIS <137741076+awaisstar@users.noreply.github.com>
2024-05-07 21:52:05 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        '3dspin': {
+          '0%': { transform: 'rotateY(0deg)' },
+          '100%': { transform: 'rotateY(360deg)' },
+        },
+      },
+      animation: {
+        '3dspin': '3dspin 15s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
